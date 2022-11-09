692294389aebddc345bc8b0237fcc6877c18c1d4
Aleksandra Rolka <arolka@interia.com>
2022-11-08 17:22:24 -0800

    Update Program.cs

--- WebApplication3/WebApplication3/Program.cs
+++ WebApplication3/WebApplication3/Program.cs
@@ -2,6 +2,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 var configuration = (IConfiguration)app.Services.GetService(typeof(IConfiguration))!;
-
-app.MapGet("/", () => $"FALING TEST! Value: {configuration.GetSection("test").Value}");
+app.MapGet("/", () => $"Hello World! Value: {configuration.GetSection("test").Value}");
 app.Run();
